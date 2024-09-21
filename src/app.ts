77d836914eb0bd2f3f70e8b1443f1337a547ae46
Rothiii <drome.emord@gmail.com>
2024-09-21 23:54:23 +0800

    feat: auth

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,24 @@ import express, { Express } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import { ErrorMiddleware } from "./middlewares";
+import { authRoute } from "./features/auth";
 
 dotenv.config();
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api/v1/auth", authRoute);
 
 app.use(ErrorMiddleware.notFound);
 app.use(ErrorMiddleware.returnError);
